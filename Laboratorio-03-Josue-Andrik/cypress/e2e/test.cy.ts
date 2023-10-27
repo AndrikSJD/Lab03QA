78788b2bb833d999ca8fdc2906cf60f1634b54a3
Andrik Solano <andriksj2208@gmail.com>
2023-10-27 14:26:09 -0600

    Casos de Prueba Terminados

--- Laboratorio-03-Josue-Andrik/cypress/e2e/test.cy.ts
+++ Laboratorio-03-Josue-Andrik/cypress/e2e/test.cy.ts
@@ -1,22 +1,27 @@
-// describe('Agregar a Carrito de compras', function () {
-//     beforeEach(function () {
-//         Cypress.on('uncaught:exception', (err, runnable) => {
-//             return false
-//         })
-//         cy.viewport(1920, 1080)
-//         cy.visit('https://www.decasa.cr/producto/adorno-elefante-dorado-4/');
-//         cy.wait(200);
-//     });
+describe('Agregar a Carrito de compras', function () {
+    beforeEach(function () {
+        Cypress.on('uncaught:exception', (err, runnable) => {
+            return false
+        })
+        cy.viewport(1920, 1080)
+        cy.visit('https://www.decasa.cr/producto/adorno-elefante-dorado-4/');
+        cy.wait(200);
+    });
 
-//     it('Agregar al carrito', function () {
-//         cy.get('.single_add_to_cart_button').click();
-//         cy.wait(200);
-//         cy.get('.mini-cart-counter').should('have.text', '1');
+    it('Agregar al carrito', function () {
+        cy.get('.single_add_to_cart_button').click();
+        cy.wait(400);
+        cy.get('.mini-cart-counter').should('have.text', '1');
 
-//     });
-// });
+    });
+    afterEach(function () {
+        cy.reload();
 
-describe('Eliminar de Carrito de compras', function () {
+    });
+
+});
+
+describe('Carrito de compras funciones', function () {
     beforeEach(function () {
         Cypress.on('uncaught:exception', (err, runnable) => {
             return false
@@ -25,12 +30,44 @@ describe('Eliminar de Carrito de compras', function () {
         cy.visit('https://www.decasa.cr/producto/adorno-elefante-dorado-4/');
         cy.wait(200);
         cy.get('.single_add_to_cart_button').click();
-        cy.wait(200);
+        cy.wait(400);
     });
 
     it('Eliminar del carrito', function () {
-        cy.get('div[class="menu-extra"]')
+        cy.get('div[class="menu-extra"]').find('ul').find('li').find('a').contains('1').click();
         cy.wait(200);
+        cy.xpath('/html/body/div[1]/div[4]/div/div/div/main/article/div/div/form/table/tbody/tr[1]/td[6]/a').click();
+        cy.wait(200);
+        cy.get('div.woocommerce-notices-wrapper').find('p').should('have.text', 'Tu carrito está vacío.');
+    });
+
+    it('ir a pagar ', function (){
+
+        cy.get('div[class="menu-extra"]').find('ul').find('li').find('a').contains('1').click();
+        cy.wait(200);
+        cy.get('div[class="wc-proceed-to-checkout"]').find('a').contains('Pagar').click()
+        cy.wait(200);
+        cy.get('h1').should('have.text', 'Pago');
+
+      })
+
+    it('Actualizar cantidad', function () {
+        cy.get('div[class="menu-extra"]').find('ul').find('li').find('a').contains('1').click();
+        cy.wait(200);
+        cy.get('input[type="number"]').clear().type('2');
+        cy.wait(200);
+        cy.xpath('/html/body/div[1]/div[4]/div/div/div/main/article/div/div/form/table/tbody/tr[2]/td/input[1]').click().then(() => {
+            cy.wait(200);
+            cy.xpath('/html/body/div[1]/div[4]/div/div/div/main/article/div/div/form/table/tbody/tr[1]/td[5]/span/bdi').should('have.text', '₡50,200');
+
+        });
 
     });
+
+    afterEach(function () {
+        cy.reload();
+
+    });
+    
+
 });
